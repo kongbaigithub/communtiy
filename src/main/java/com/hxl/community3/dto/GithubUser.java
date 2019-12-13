208fdb6e51d422bbb467cd3cdd58f973c5285847
kongbaigithub <1057267320@qq.com>
2019-12-13 23:35:20 +0800

    登陆设置

---
+++ src/main/java/com/hxl/community3/dto/GithubUser.java
@@ -0,0 +1,32 @@
+package com.hxl.community3.dto;
+
+public class GithubUser {
+
+    private String name;
+    private Long id;
+    private String biol;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBiol() {
+        return biol;
+    }
+
+    public void setBiol(String biol) {
+        this.biol = biol;
+    }
+}
